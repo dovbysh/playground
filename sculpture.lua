700c3d6bfc6ff5f9963fa0ee851048ff78acd01f
Andrey Dovbyshko <dovbysh@gmail.com>
2021-01-25 23:43:54 +0300

    sculpture improvements

--- sculpture.lua
+++ sculpture.lua
@@ -17,7 +17,7 @@ minetest.register_node("playground:sculpter", {
 })
 
 function sculprtureUse(itemstack, user, pointed_thing)
-    minetest.chat_send_all("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz: " .. debug.getinfo(1).currentline .. dump(pointed_thing))
+    --minetest.chat_send_all("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz: " .. debug.getinfo(1).currentline .. dump(pointed_thing))
     local ob = {}
     local pos = user:get_pos()
     minetest.chat_send_all(dump(pointed_thing))
@@ -27,10 +27,14 @@ function sculprtureUse(itemstack, user, pointed_thing)
         minetest.set_node(pointed_thing.above, { name = "mapgen_stone" })
         for di = 1, 10 do
             minetest.set_node(vector.add(pointed_thing.above, { x = 0, y = di, z = 0 }), { name = "mapgen_stone" })
-            minetest.set_node(vector.add(pointed_thing.above, { x = di, y = di, z = 0 }), { name = "mapgen_stone" })
-            minetest.set_node(vector.add(pointed_thing.above, { x = -di, y = di, z = 0 }), { name = "mapgen_stone" })
-            minetest.set_node(vector.add(pointed_thing.above, { x = 0, y = di, z = di }), { name = "mapgen_stone" })
-            minetest.set_node(vector.add(pointed_thing.above, { x = 0, y = di, z = -di }), { name = "mapgen_stone" })
+            for dy = 0, 10, 3 do
+                for i = 0, 1 do
+                    minetest.set_node(vector.add(pointed_thing.above, { x = di, y = di + dy, z = 0 + i * di }), { name = "mapgen_stone" })
+                    minetest.set_node(vector.add(pointed_thing.above, { x = -di, y = di + dy, z = 0 + i * di }), { name = "mapgen_stone" })
+                    minetest.set_node(vector.add(pointed_thing.above, { x = 0 - i * di, y = di + dy, z = di }), { name = "mapgen_stone" })
+                    minetest.set_node(vector.add(pointed_thing.above, { x = 0 - i * di, y = di + dy, z = -di }), { name = "mapgen_stone" })
+                end
+            end
         end
     end
     return itemstack
